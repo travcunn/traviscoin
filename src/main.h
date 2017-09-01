77ecf5e03d49bc971c25ba69c038c9fa3c9bd802
Travis Cunningham <tcunningham@smartfile.com>
2017-09-01 15:47:42 -0400

    Update main.h

--- src/main.h
+++ src/main.h
@@ -30,9 +30,9 @@ static const unsigned int MAX_BLOCK_SIZE = 1000000;
 static const unsigned int MAX_BLOCK_SIZE_GEN = MAX_BLOCK_SIZE/2;
 static const unsigned int MAX_BLOCK_SIGOPS = MAX_BLOCK_SIZE/50;
 static const unsigned int MAX_ORPHAN_TRANSACTIONS = MAX_BLOCK_SIZE/100;
-static const int64 MIN_TX_FEE = 0.1 * CENT;
-static const int64 MIN_RELAY_TX_FEE = 0.1 * CENT;
-static const int64 MAX_MONEY = 200000 * COIN;
+static const int64 MIN_TX_FEE = 0;
+static const int64 MIN_RELAY_TX_FEE = 0;
+static const int64 MAX_MONEY = 200000000 * COIN;
 static const int64 MAX_MINT_PROOF_OF_WORK = 9999 * COIN;
 static const int64 MIN_TXOUT_AMOUNT = MIN_TX_FEE;
 inline bool MoneyRange(int64 nValue) { return (nValue >= 0 && nValue <= MAX_MONEY); }
